fb7132bdb10f3e485d94aca669d6fe0cfb8ebd45
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2025-01-22 11:50:00 -0600

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the image name and tag
+IMAGE_NAME="bhonagiri/sound_anomaly"
+IMAGE_TAG="latest"
+
+# Pull the image from Docker Hub
+echo "Pulling image $IMAGE_NAME:$IMAGE_TAG from Docker Hub..."
+docker pull $IMAGE_NAME:$IMAGE_TAG
+
+# Run the image
+echo "Running the Docker image..."
+docker run -it --rm $IMAGE_NAME:$IMAGE_TAG
+
+# End of script
